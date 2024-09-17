283330a2ba870f774a6bb7dc3bc160c389c6a782
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-09-17 09:56:10 +0530

    Add files via upload

---
+++ Array/TraversingArrayElementwithForLoop.java
@@ -0,0 +1,11 @@
+package ARRAY;
+
+public class TraversingArrayElementwithForLoop {
+    public static void main(String[] args) {
+        int[] numbers = {10,20,30,40,50};
+        for (int i=0; i < numbers.length; i++){
+            System.out.println("Element at Index " + numbers[i]);
+        }
+    }
+
+}
