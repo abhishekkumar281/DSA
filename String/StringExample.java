d27600b48251df6a0da300ba174c96836d83b73a
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-09-28 14:53:30 +0530

    Add files via upload

---
+++ String/StringExample.java
@@ -0,0 +1,9 @@
+package STRING;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String str = new String("Example");
+        String str1 = "Example";
+        System.out.println(str==(str1));
+    }
+}
