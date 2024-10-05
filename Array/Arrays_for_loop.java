d2188343c41287b49042c44066d3e1399caad892
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-10-05 23:56:12 +0530

    Add files via upload

---
+++ Array/Arrays_for_loop.java
@@ -0,0 +1,21 @@
+public class Arrays_for_loop {
+    public static void main(String[] args) {
+        int [] marks ={89,83,58,96,76};
+    //    float [] marks ={87.2f,83.7f,98.7f,38.6f,94.3f,};
+        String [] student ={"abhishek","niraj","rajan","subham","munna"};
+        System.out.println(student.length);
+        System.out.println(student[2]);
+        int [] score={73,68,82,31,22,};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+    //    Displaying the array( for loop):
+        for (int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+    //  Displaing the array for loop in reverse ord er;
+                System.out.println(marks[i]);
+            }
+        }
+    }
