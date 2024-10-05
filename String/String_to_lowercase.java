a2fbaaa88b7495863f4ebc3363c3904fc2e98db0
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-10-05 23:57:24 +0530

    Add files via upload

---
+++ String/String_to_lowercase.java
@@ -0,0 +1,7 @@
+public class String_to_lowercase {
+    public static void main(String[] args) {
+        String name ="Abhishek Kumar";
+        name =name.toLowerCase();
+        System.out.println(name);
+    }
+}
