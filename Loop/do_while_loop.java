1fe396f5222dc1292c80f0d500137c534ce4fb42
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-10-06 23:23:48 +0530

    Add files via upload

---
+++ Loop/do_while_loop.java
@@ -0,0 +1,17 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        //    int a = 0;
+        //    while (a<5){
+        //        System.out.println(a);
+        //        a++;
+        //    }
+
+    //    int b = 10;
+        int b =0;
+        do {
+            System.out.println(b);
+            b++;
+        } while (b < 5);
+    }
+}
+
