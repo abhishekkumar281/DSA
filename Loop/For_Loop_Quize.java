cfb750a68517484a990379d1dd25cf3865635e34
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-10-06 23:25:08 +0530

    Add files via upload

---
+++ Loop/For_Loop_Quize.java
@@ -0,0 +1,16 @@
+public class For_Loop_Quize {
+    public static void main(String[] args) {
+        // 2i= even number= 0 2 4 6 8 10
+        //2i+1= odd number= 1  3 5 7 9
+
+               // FOR EVEN NUMBER
+
+        int n =5;
+        for (int i=0; i<n; i++){
+            System.out.println(2*i+1);
+
+               // FOR ODD NUMBER
+            System.out.println(2*i);
+        }
+    }
+}
