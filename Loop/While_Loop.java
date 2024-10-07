5506894741d92e5d2b7cab8c7c0b2868798eef5e
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-10-07 22:32:42 +0530

    Add files via upload

---
+++ Loop/While_Loop.java
@@ -0,0 +1,15 @@
+public class While_Loop {
+    public static void main(String[] args) {
+        int i= 0;
+        while (i<=3){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish running time loop");
+
+
+           while (true){
+              System.out.println("abhishek");
+           }
+    }
+}
