cd489acbecf3fd0db2239de08a1ade1df9c29784
abhishekkumar281 <145953543+abhishekkumar281@users.noreply.github.com>
2024-10-08 21:18:57 +0530

    Add files via upload

---
+++ Loop/Decrement_for_loop.java
@@ -0,0 +1,19 @@
+public class Decrement_for_loop {
+    public static void main(String[] args) {
+    //    for(int i=5; i>0; i--){
+    //        System.out.println(i);
+    //}
+
+
+
+
+
+        //  QUIZE
+
+
+        for(int i=5; i!=0; i--){
+            System.out.println(i);
+        }
+
+    }
+}
